0e3d9785be7758456048bd263b518751a73e757d
Jake <jakeescalada26@gmail.com>
2024-08-22 11:47:44 +0800

    Unang kababalaghan

---
+++ main.py
@@ -0,0 +1,15 @@
+first_number = int(input("Enter first number: "))
+second_number = int(input("Enter second number: "))
+
+if first_number == second_number:
+    print(f"{first_number} is equals to {second_number}")
+elif first_number > second_number:
+    print(f"{first_number} is greater than {second_number}")
+elif first_number < second_number:
+    print(f"{first_number} is less than {second_number}")
+
+name1 = input("Enter classmate name 1: ")
+name2 = input("Enter classmate name 2: ")
+name3 = input("Enter classmate name 3: ")
+
+print(f"Your classmates are: {name1}, {name2}, & {name3}.")
